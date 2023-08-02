85909b2bfe031f09d49c8f777ff892c85d3af4fa
bahittinn <bahittin84@gmail.com>
2023-08-02 20:18:17 +0300

    words added

---
+++ Word-Scramble/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Word-Scramble
+//
+//  Created by Bahittin on 2.08.2023.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var allWords = [String]()
+    var usedWords = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
